81599d4c47478a46d612542301e75c24cfd7840f
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-11-25 20:28:57 -0600

    check if `isHandlePtr != nil` before parsing

--- tsgo/write_cgo.go
+++ tsgo/write_cgo.go
@@ -595,17 +595,19 @@ func (g *PackageGenerator) writeCGoFieldAccessor(gi *strings.Builder, gh *string
 
 	fnSB.WriteString("}\n\n")
 
-	for _, fa := range f.fieldAccessors {
-		var accessorSB strings.Builder
-		accessorSB.WriteString("_GET_")
-		accessorSB.WriteString(*f.isHandleFn)
-		accessorSB.WriteString("_")
-		accessorSB.WriteString(*fa.name)
-		name := accessorSB.String()
-		fa.fnName = &name
-		fnSB.WriteString(g.writeCGoFieldAccessor(gi, gh, ec, ci, fmtr, fa, pkgName, *f.isHandleFn))
+	if f.isHandleFn != nil {
+		for _, fa := range f.fieldAccessors {
+			var accessorSB strings.Builder
+			accessorSB.WriteString("_GET_")
+			accessorSB.WriteString(*f.isHandleFn)
+			accessorSB.WriteString("_")
+			accessorSB.WriteString(*fa.name)
+			name := accessorSB.String()
+			fa.fnName = &name
+			fnSB.WriteString(g.writeCGoFieldAccessor(gi, gh, ec, ci, fmtr, fa, pkgName, *f.isHandleFn))
+		}
+		fnSB.WriteString(g.writeDisposeStruct(f.disposeHandle))
 	}
-	fnSB.WriteString(g.writeDisposeStruct(f.disposeHandle))
 
 	return fnSB.String()
 }
