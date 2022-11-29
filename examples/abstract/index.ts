47568c934e86cade07eae0e4e76949b0346ef40f
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2022-11-29 14:06:09 -0600

    remove incorrectly gen native fn exports

--- examples/abstract/index.ts
+++ examples/abstract/index.ts
@@ -83,13 +83,14 @@ export interface DemoStruct3 {
 
 export const {
   symbols: {
-    _Int32ArgTest,
-    _Int64ArgTest,
-    _IntTest,
-    genDisposePtr,
+    _TestMap,
     arraySize,
-    _Uint64ArgTest,
-    _TestStruct2,
+    _Int32ArgTest,
+    _Uint64ArrayTest,
+    _StringTest,
+    _Float32ArgTest,
+    _Int64ArgTest,
+    _TestStruct,
     _DISPOSE_Struct,
     _INIT_StructBar,
     _SET_StructBar_Field,
@@ -119,39 +120,19 @@ export const {
     _INIT_DemoStruct3,
     _SET_DemoStruct3_AnotherArray,
     _GET_DemoStruct3_AnotherArray,
-    _TestMap,
-    _Int32ArrayTest,
-    _Int64ArrayTest,
-    _Float32ArgTest,
     _Float32ArrayTest,
+    _Float64ArrayTest,
+    _Int64ArrayTest,
     _Uint32ArrayTest,
     _Uint32ArgTest,
-    _Float64ArgTest,
-    _TestStruct,
-    _Float64ArrayTest,
-    _Uint64ArrayTest,
-    _StringTest
+    genDisposePtr,
+    _Int32ArrayTest,
+    _Uint64ArgTest,
+    _TestStruct2,
+    _IntTest,
+    _Float64ArgTest
   }
 } = dlopen(import.meta.dir + '/abstract/gen_bindings.dylib', {
-  _IntTest: {
-    args: [FFIType.cstring],
-    returns: FFIType.int
-  },
-  genDisposePtr: {
-    returns: FFIType.ptr
-  },
-  arraySize: {
-    args: [FFIType.ptr],
-    returns: FFIType.u64_fast
-  },
-  _Int32ArgTest: {
-    args: [FFIType.ptr, FFIType.u64_fast],
-    returns: FFIType.ptr
-  },
-  _Int64ArgTest: {
-    args: [FFIType.ptr, FFIType.u64_fast],
-    returns: FFIType.ptr
-  },
   _Int32ArrayTest: {
     args: [FFIType.cstring],
     returns: FFIType.ptr
@@ -160,7 +141,18 @@ export const {
     args: [FFIType.cstring],
     returns: FFIType.ptr
   },
-  _Float32ArgTest: {
+  _Uint32ArrayTest: {
+    args: [FFIType.cstring],
+    returns: FFIType.ptr
+  },
+  _Uint32ArgTest: {
+    args: [FFIType.ptr, FFIType.u64_fast],
+    returns: FFIType.ptr
+  },
+  genDisposePtr: {
+    returns: FFIType.ptr
+  },
+  _Float64ArgTest: {
     args: [FFIType.ptr, FFIType.u64_fast],
     returns: FFIType.ptr
   },
@@ -274,20 +266,20 @@ export const {
     args: [FFIType.ptr],
     returns: FFIType.ptr
   },
+  _IntTest: {
+    args: [FFIType.cstring],
+    returns: FFIType.int
+  },
+  _Int32ArgTest: {
+    args: [FFIType.ptr, FFIType.u64_fast],
+    returns: FFIType.ptr
+  },
   _TestMap: {
     returns: FFIType.cstring
   },
-  _Float32ArrayTest: {
-    args: [FFIType.cstring],
-    returns: FFIType.ptr
-  },
-  _Uint32ArrayTest: {
-    args: [FFIType.cstring],
-    returns: FFIType.ptr
-  },
-  _Uint32ArgTest: {
-    args: [FFIType.ptr, FFIType.u64_fast],
-    returns: FFIType.ptr
+  arraySize: {
+    args: [FFIType.ptr],
+    returns: FFIType.u64_fast
   },
   _Float64ArrayTest: {
     args: [FFIType.cstring],
@@ -300,13 +292,21 @@ export const {
   _StringTest: {
     returns: FFIType.cstring
   },
-  _Float64ArgTest: {
+  _Float32ArgTest: {
+    args: [FFIType.ptr, FFIType.u64_fast],
+    returns: FFIType.ptr
+  },
+  _Int64ArgTest: {
     args: [FFIType.ptr, FFIType.u64_fast],
     returns: FFIType.ptr
   },
   _TestStruct: {
     returns: FFIType.ptr
   },
+  _Float32ArrayTest: {
+    args: [FFIType.cstring],
+    returns: FFIType.ptr
+  }
 })
 
 const registry = new FinalizationRegistry((disp: { cb: (ptr: number) => void; ptr: number}) => {
@@ -391,7 +391,7 @@ export class _StructBar {
 
   set StructField(val: StructBar["StructField"]) {
     const parsed_value = val instanceof _DemoStruct ? val : _DemoStruct.init(val);
-    _SET_StructBar_StructField(this._ptr, val.ptr);
+    _SET_StructBar_StructField(this._ptr, parsed_value.ptr);
   }
 
   static init(struct: StructBar): _StructBar {
@@ -444,7 +444,7 @@ export class _DemoStruct {
 
   set FieldToAnotherStruct(val: DemoStruct["FieldToAnotherStruct"]) {
     const parsed_value = val instanceof _DemoStruct2 ? val : _DemoStruct2.init(val);
-    _SET_DemoStruct_FieldToAnotherStruct(this._ptr, val.ptr);
+    _SET_DemoStruct_FieldToAnotherStruct(this._ptr, parsed_value.ptr);
   }
 
   static init(struct: DemoStruct): _DemoStruct {
@@ -493,7 +493,7 @@ export class _DemoStruct2 {
 
   set BacktoAnotherStruct(val: DemoStruct2["BacktoAnotherStruct"]) {
     const parsed_value = val instanceof _DemoStruct3 ? val : _DemoStruct3.init(val);
-    _SET_DemoStruct2_BacktoAnotherStruct(this._ptr, val.ptr);
+    _SET_DemoStruct2_BacktoAnotherStruct(this._ptr, parsed_value.ptr);
   }
 
   static init(struct: DemoStruct2): _DemoStruct2 {
